2a01d67dda54cbb0a01d649a772464706a9d00ac
tahery1991 <tahery1991@gmail.com>
2026-08-11 08:06:16 +0330

    Configure PWA for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  // مسیر Repository در GitHub Pages
+  base: '/PCOS-app/',
+
   plugins: [
     vue(),
 
@@ -15,24 +18,26 @@ export default defineConfig({
         name: 'PCOS Smart App',
         short_name: 'PCOS Smart',
         description: 'سیستم هوشمند مدیریت و پیش‌بینی سندرم تخمدان پلی‌کیستیک',
+
         theme_color: '#0b1f3a',
         background_color: '#0b1f3a',
+
         display: 'standalone',
         orientation: 'portrait',
 
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/PCOS-app/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/PCOS-app/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/PCOS-app/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
